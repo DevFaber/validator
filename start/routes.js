37dfb20a756a079884eab4f678d7eba62aabb765
Fabricio Valerio <fvr.valerio@gmail.com>
2019-12-01 11:48:07 -0400

    bckValidatorV2

--- start/routes.js
+++ start/routes.js
@@ -5,14 +5,17 @@ const Route = use('Route')
 Route.post('validations', 'ValidationController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
-
 Route.post('sessions', 'SessionController.store')
 
-Route.post('companies', 'CompanyController.store')
-Route.get('companies', 'CompanyController.index')
+Route.group(() => {
+  Route.post('companies', 'CompanyController.store')
+  Route.get('companies', 'CompanyController.index')
 
-Route.post('users', 'UserController.store')
-Route.get('users', 'UserController.index')
+  Route.post('users', 'UserController.store')
+  Route.get('users', 'UserController.index')
 
-Route.get('listvalidations', 'ValidationController.index')
-Route.get('validations', 'ValidationController.index')
+  Route.get('listvalidations', 'ValidationController.index')
+  Route.get('validations', 'ValidationController.index')
+  Route.post('files', 'FileController.store')
+  Route.get('files/:id', 'FileController.show')
+}).middleware(['auth'])
