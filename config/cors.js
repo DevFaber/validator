b67a2e3f254bb885bdc717ccfacced137c6f1566
Fabricio Valerio <fvr.valerio@gmail.com>
2020-01-14 11:15:16 -0400

    Cors2

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: ['https://api.validaonline.net'],
+  origin: ['https://api.validaonline.net', 'https://www.validaonline.net'],
 
   /*
   |--------------------------------------------------------------------------
